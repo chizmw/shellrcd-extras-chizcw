07fc628bf344cea83f5ab90932ca583a77ed922d
Chisel <chisel.malik-wright@zoopla.co.uk>
2020-05-13 17:05:17 +0100

    FIX _agnostic/20.alias-zoopla.tf
    '~' does NOT expand in strings!!

--- _agnostic/20.alias-zoopla.tf
+++ _agnostic/20.alias-zoopla.tf
@@ -1,3 +1,3 @@
-if [ -f "~/development/devops/terraform/terraform" ]; then
-    alias tf="/Users/chisel/development/devops/terraform/terraform"
+if [ -f "$HOME/development/devops/terraform/terraform" ]; then
+    alias tf="$HOME/development/devops/terraform/terraform"
 fi
