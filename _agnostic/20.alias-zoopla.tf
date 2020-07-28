e7c7410c5386e24603df3bfdcbd70b887e4c24ff
Chisel <chisel.malik-wright@zoopla.co.uk>
2020-07-28 10:01:06 +0100

    Add TF_WRAPPER_SCRIPT to _agnostic/20.alias-zoopla.tf

--- _agnostic/20.alias-zoopla.tf
+++ _agnostic/20.alias-zoopla.tf
@@ -1,3 +1,7 @@
 if [ -f "$HOME/development/devops/terraform/terraform" ]; then
+    # this is the most useful
     alias tf="$HOME/development/devops/terraform/terraform"
+
+    # this is occasionally useful
+    export TF_WRAPPER_SCRIPT="$HOME/development/devops/terraform/terraform"
 fi
