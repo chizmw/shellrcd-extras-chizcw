16e70779892bec3855578eda899cd762c3f39511
Chisel <chisel.malik-wright@zoopla.co.uk>
2020-09-28 14:52:48 +0100

    Pull down tfsso in 20.alias-zoopla.tf

--- _agnostic/20.alias-zoopla.tf
+++ _agnostic/20.alias-zoopla.tf
@@ -1,6 +1,13 @@
 if [ -f "$HOME/development/devops/terraform/terraform" ]; then
     # this is the most useful
     alias tf="$HOME/development/devops/terraform/terraform"
+    if ! type tfsso &>/dev/null; then
+      printf "$(git archive --remote=ssh://git@gitlab.zoopla.co.uk/devops/terraform-wrapper.git HEAD install-sso | tar -xO)" |sed 1,1d \
+        >> ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+      chmod 0755 ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+      source ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+      declare -f tfsso
+    fi
 
     # this is occasionally useful
     export TF_WRAPPER_SCRIPT="$HOME/development/devops/terraform/terraform"
