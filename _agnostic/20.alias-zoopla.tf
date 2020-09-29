309115b27b32f9d4926f0f6ad2683f0badeb5c21
Chisel <chisel.malik-wright@zoopla.co.uk>
2020-09-29 10:34:48 +0100

    Support tfsso a bit better

--- _agnostic/20.alias-zoopla.tf
+++ _agnostic/20.alias-zoopla.tf
@@ -1,12 +1,17 @@
 if [ -f "$HOME/development/devops/terraform/terraform" ]; then
-    # this is the most useful
-    alias tf="$HOME/development/devops/terraform/terraform"
     if ! type tfsso &>/dev/null; then
       printf "$(git archive --remote=ssh://git@gitlab.zoopla.co.uk/devops/terraform-wrapper.git HEAD install-sso | tar -xO)" |sed 1,1d \
         >> ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
       chmod 0755 ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
       source ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
       declare -f tfsso
+    fi
+
+    # this is the most useful
+    if [ -z "$USE_AWS_SSO" ]; then
+        alias tf="$HOME/development/devops/terraform/terraform"
+    else
+        alias tf=tfsso
     fi
 
     # this is occasionally useful
