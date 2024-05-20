58e400a64fa8d6e2dcae9a7dedaca781543069a0
Chisel <chisel@chizography.net>
2024-05-20 09:26:04 +0100

    chore: comment out contents of _agnostic/20.alias.zoopla.tf

--- _agnostic/20.alias.zoopla.tf
+++ _agnostic/20.alias.zoopla.tf
@@ -1,21 +1,21 @@
-#!/bin/bash
-if [ -f "$HOME/development/devops/terraform/terraform" ]; then
-    if ! type tfsso &>/dev/null; then
-      # overwrite any existing file(s)
-      printf "$(git archive --remote=ssh://git@gitlab.zoopla.co.uk/devops/terraform-wrapper.git HEAD install-sso | tar -xO)" |sed 1,1d \
-        > ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
-      chmod 0755 ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
-      source ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
-      declare -f tfsso
-    fi
+# #!/bin/bash
+# if [ -f "$HOME/development/devops/terraform/terraform" ]; then
+#     if ! type tfsso &>/dev/null; then
+#       # overwrite any existing file(s)
+#       printf "$(git archive --remote=ssh://git@gitlab.zoopla.co.uk/devops/terraform-wrapper.git HEAD install-sso | tar -xO)" |sed 1,1d \
+#         > ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+#       chmod 0755 ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+#       source ~/.shellrc.d/_agnostic/19.alias.zoopla.tfsso
+#       declare -f tfsso
+#     fi
 
-    # this is the most useful
-    if [ -z "$USE_AWS_SSO" ]; then
-        alias tf="$HOME/development/devops/terraform/terraform"
-    else
-        alias tf=tfsso
-    fi
+#     # this is the most useful
+#     if [ -z "$USE_AWS_SSO" ]; then
+#         alias tf="$HOME/development/devops/terraform/terraform"
+#     else
+#         alias tf=tfsso
+#     fi
 
-    # this is occasionally useful
-    export TF_WRAPPER_SCRIPT="$HOME/development/devops/terraform/terraform"
-fi
+#     # this is occasionally useful
+#     export TF_WRAPPER_SCRIPT="$HOME/development/devops/terraform/terraform"
+# fi
